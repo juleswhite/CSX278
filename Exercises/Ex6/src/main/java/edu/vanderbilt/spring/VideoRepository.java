68b28187d3686fbeabd6e968f0d3edfa228eab02
Juan Sebastian Hoyos Ayala <juan.s.hoyos@outlook.com>
2016-09-29 11:41:55 -0500

    Remove unnecessary dependencies

--- Exercises/Ex6/src/main/java/edu/vanderbilt/spring/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.vanderbilt.spring;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VideoRepository extends CrudRepository<Video, Long>{
-
-}
